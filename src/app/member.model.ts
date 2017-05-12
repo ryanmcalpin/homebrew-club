5873959a84208704b440f1d5fe6a2d01923946e5
Ryan McAlpin <rhmcalpin@gmail.com>
2017-05-12 14:06:10 -0700

    add member class

---
+++ src/app/member.model.ts
@@ -0,0 +1,3 @@
+export class Member {
+  constructor(public firstName: string, public lastName: string, public dateJoined: string, public yearStarted: number, public bio: string, public favoriteStyles: string, public city: string, public state: string, public country: string, public zip: string) {}
+}
